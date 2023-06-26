59b2cec912eea782679a2f96e6993bb639be06b3
Ardit Hunçi <95981305+ardithunci@users.noreply.github.com>
2023-06-27 00:49:50 +0200

    code optimization

--- 11. Drum-Kit/script.js
+++ 11. Drum-Kit/script.js
@@ -1,5 +1,5 @@
 const clickPlay = document.querySelectorAll('.key');
-
+//cursor
 for (let click of clickPlay){
     click.addEventListener("click", ()=>{
         const active = document.querySelector('.playing');
@@ -12,48 +12,17 @@ for (let click of clickPlay){
         audio.play();
     })
 }
-
-window.addEventListener("keyup",(e)=>{
-    switch(e.keyCode){
-       case 65:
-        play(e.keyCode)
-       break;
-       case 83:
-        play(e.keyCode)
-       break;
-       case 68:
-        play(e.keyCode)
-       break;
-       case 70:
-        play(e.keyCode)
-       break;
-       case 71:
-        play(e.keyCode)
-       break;
-       case 72:
-        play(e.keyCode)
-       break;
-       case 74:
-        play(e.keyCode) 
-       break;
-       case 75:
-        play(e.keyCode)
-       break;
-       case 76:
-        play(e.keyCode)
-       break;
-    }
-
-    function play(keyCode){
+//key
+window.addEventListener("keydown",(e)=>{
         const active = document.querySelector('.playing');
         if (active){
             active.classList.remove("playing")
         }
-       const key = document.querySelector(`div[data-key="${keyCode}"]`)
+       const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+       if (!key) return;
        key.classList.toggle("playing"); 
-       const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+       const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
        audio.currentTime = 0;
        audio.play();
-    }
 })
 
