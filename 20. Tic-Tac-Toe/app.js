ec3190297758bf07f7d97f91b6b8209d2e5f9c12
Ardit Hunçi <95981305+ardithunci@users.noreply.github.com>
2023-07-10 14:34:01 +0200

    final commit

--- 20. Tic-Tac-Toe/app.js
+++ 20. Tic-Tac-Toe/app.js
@@ -16,7 +16,6 @@ squares.forEach((_cell, index)=>{
    
 })
 
-
 function addGo(e){
     const goDisplay = document.createElement('div');
     goDisplay.classList.add(go);
@@ -28,5 +27,32 @@ function addGo(e){
 }
 
 function checkScore() {
-    
-}
+    const allSquares = document.querySelectorAll('.square');
+    const winningCombos = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+
+    winningCombos.forEach(array => {
+        const circleWins = array.every(cell => 
+            allSquares[cell].firstChild?.classList.contains('circle'));
+            if (circleWins){
+                info.textContent = "Circle Wins!";
+                allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                return;
+            }
+    })
+
+    winningCombos.forEach(array => {
+        const crossWins = array.every(cell => 
+            allSquares[cell].firstChild?.classList.contains('cross'));
+            if (crossWins){
+                info.textContent = "Cross Wins!";
+                allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                return;
+            }
+    })
+} 
+
+
